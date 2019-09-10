9d6a6cf00c3619be41750848dccc49bf3ecac7e4
Alex Woods <alex.woods89@hotmail.co.uk>
2019-09-10 16:18:59 +0100

    lab

---
+++ src/app.js
@@ -0,0 +1,22 @@
+import Vue from 'vue';
+
+document.addEventListener('DOMContentLoaded', () => {
+    new Vue({
+        el: '#app',
+        data: {
+            countries: [],
+            
+        },
+        mounted() {
+            this.fetchCountries();
+        },
+        methods: {
+        fetchCountries: function() {
+          fetch('https://restcountries.eu/rest/v2/all')
+          .then(response => response.json())
+          .then(data => this.countries = data)
+          .catch(console.error);
+        }
+        }
+    })
+})
